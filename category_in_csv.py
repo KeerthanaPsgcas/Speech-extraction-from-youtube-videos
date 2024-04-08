39fe3d2e96914a098e9fc4396c0ae5a84a626441
KeerthanaPsgcas <20mss019@psgcas.ac.in>
2024-04-08 16:26:45 +0530

    added category in csv

--- category_in_csv.py
+++ category_in_csv.py
@@ -1,5 +1,7 @@
+"""EXTRA FEATURE IN CSV FILE: CATEGORY OF THE VIDEO
 
-"""In this app, we will create a Streamlit app that can fetch the transcript of a YouTube video 
+
+In this app, we will create a Streamlit app that can fetch the transcript of a YouTube video 
 or a playlist of videos. We will use the `pytube` library to fetch the video details.
 The `youtube_transcript_api` library will be used to fetch the transcript of the video.
 We will also use the `googletrans` library to translate the transcript to English if it is not in English.
@@ -7,8 +9,10 @@ The app will take a YouTube video URL or a playlist URL as input and display the
  provide an option to download the transcript as a CSV file.
 """
 
+
+
 import streamlit as st
-import re
+from googleapiclient.discovery import build
 from googletrans import Translator
 from youtube_transcript_api import YouTubeTranscriptApi
 from pytube import YouTube
@@ -40,6 +44,25 @@ def get_transcript(video_id):
             continue
     return ""
 
+def get_video_category_title(video_id):
+    youtube = build('youtube', 'v3', developerKey='AIzaSyDbXBASt0vgzaX9YI581LDD2FXe3wFs_Hc')
+    request = youtube.videos().list(part='snippet', id=video_id)
+    response = request.execute()
+    if 'items' in response:
+        category_id = response['items'][0]['snippet']['categoryId']
+        category_title = fetch_category_title(category_id)
+        return category_title
+    return None
+
+def fetch_category_title(category_id):
+    youtube = build('youtube', 'v3', developerKey='AIzaSyDbXBASt0vgzaX9YI581LDD2FXe3wFs_Hc')
+    request = youtube.videoCategories().list(part='snippet', id=category_id)
+    response = request.execute()
+    if 'items' in response:
+        category_title = response['items'][0]['snippet']['title']
+        return category_title
+    return None
+
 def main():
     st.title('YouTube Playlist / Video Fetcher')
     
@@ -57,16 +80,20 @@ def main():
                         yt = YouTube(video_url)
                         video_id = yt.video_id
                         video_title = yt.title
+                        video_category = get_video_category_title(video_id)
                         video_link = yt.watch_url
                         video_posted_date = yt.publish_date.strftime('%Y-%m-%d') if yt.publish_date else ''
                         video_transcript = get_transcript(video_id)
+                        
 
                         video_data.append({
                             'Video ID': video_id,
                             'Title': video_title,
+                            'Category': video_category,
                             'Link': video_link,
                             'Posted Date': video_posted_date,
                             'Transcript': video_transcript
+                            
                         })
 
                     df = pd.DataFrame(video_data)
@@ -80,23 +107,23 @@ def main():
                 yt = YouTube(url)
                 video_id = yt.video_id
                 video_title = yt.title
+                video_category = get_video_category_title(video_id)
                 video_link = yt.watch_url
                 video_posted_date = yt.publish_date.strftime('%Y-%m-%d') if yt.publish_date else ''
                 video_transcript = get_transcript(video_id)
+                
 
                 st.write('Video Details:')
                 st.write(f'Video ID: {video_id}')
                 st.write(f'Title: {video_title}')
+                st.write(f'Category: {video_category}')
                 st.write(f'Link: {video_link}')
                 st.write(f'Posted Date: {video_posted_date}')
                 st.write(f'Transcript: {video_transcript}')
+                
 
-  
-              
         except Exception as e:
             st.error('Error fetching data. Please check the URL.')
-
-
 
 if __name__ == '__main__':
     main()
